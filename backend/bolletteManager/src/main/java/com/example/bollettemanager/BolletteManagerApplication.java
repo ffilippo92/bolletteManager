3afb8d81b7dd4644335e2d283a701c586942849b
ffilippo92 <55035951+ffilippo92@users.noreply.github.com>
2025-12-02 13:00:02 +0100

    Base ok postgres e docker, caricamento base Spring backend
    configure postgres datasource

---
+++ backend/bolletteManager/src/main/java/com/example/bollettemanager/BolletteManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bollettemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolletteManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolletteManagerApplication.class, args);
+    }
+
+}
---
+++ backend/bolletteManager/src/test/java/com/example/bollettemanager/BolletteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bollettemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolletteManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
